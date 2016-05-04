277d65d22bcb1f66f0ef8243a0ccebad0508b856
Fabien CHERANCE <fabien.cherance@gmail.com>
2016-05-05 00:10:14 +0100

    Update Hack_List.R

--- Hack_List.R
+++ Hack_List.R
@@ -28,5 +28,35 @@ iskey(cars)
 iskey(cars$speed)
 sapply(cars,iskey)
 
+#keep columns by name
+data(iris)
+iris
+
+#Get names
+grep("sepal",names(iris),ignore.case=T,value=T)
+#Get column index
+grep("sepal",names(iris),ignore.case=T,value=F)
+
+iris$Length <- pmin(iris$Sepal.Length,iris$Petal.Length)
+iris$Sepal <- pmax(iris$Sepal.Length,iris$Sepal.Width)
+
+
+#caret ^ specifies that the leading string has to be empty
+grep("Length",names(iris))
+grep("^Length",names(iris))
+#dollar $ specifies that the ending string has to be empty
+grep("Sepal",names(iris))
+grep("Sepal$",names(iris))
+
+iris$var2 <- "a"
+
+#with a digit, not ending with a digit, having a non-digit
+grep("[[:digit:]]",names(iris))
+grep("[^[:digit:]]$",names(iris))
+grep("[^[:digit:]]",names(iris))
+grep("[[:upper:]]",names(iris))
+#... And so on. Check
+#?regex
+
 
 
